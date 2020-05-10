fefb8a7e44d064a14a1852cd4ca5dd24616b4240
John O'Reilly <oreillyj@gmail.com>
2020-05-10 22:44:09 +0100

    add/use Flow.collectAsState

--- app/src/main/java/com/surrus/peopleinspace/ui/MainActivity.kt
+++ app/src/main/java/com/surrus/peopleinspace/ui/MainActivity.kt
@@ -1,28 +1,27 @@
 package com.surrus.peopleinspace.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.*
-import androidx.lifecycle.lifecycleScope
 import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
 import androidx.ui.foundation.AdapterList
 import androidx.ui.foundation.Text
 import androidx.ui.layout.Column
-import androidx.ui.layout.LayoutPadding
 import androidx.ui.layout.padding
-import androidx.ui.livedata.observeAsState
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.TopAppBar
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import com.surrus.common.remote.Assignment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
+import kotlin.coroutines.CoroutineContext
 
 
 @ExperimentalCoroutinesApi
@@ -32,16 +31,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        lifecycleScope.launch {
-            peopleInSpaceViewModel.peopleInSpace.collect {
-                setContent { mainLayout(it) }
-            }
+
+        setContent {
+            mainLayout(peopleInSpaceViewModel)
         }
     }
 }
 
 @Composable
-fun mainLayout(peopleInSpace: List<Assignment>) {
+fun mainLayout(peopleInSpaceViewModel: PeopleInSpaceViewModel) {
+    val peopleState = peopleInSpaceViewModel.peopleInSpace.collectAsState(emptyList())
+
     MaterialTheme {
         Column {
             TopAppBar(
@@ -49,10 +49,9 @@ fun mainLayout(peopleInSpace: List<Assignment>) {
                     Text("People In Space")
                 }
             )
-            AdapterList(data = peopleInSpace) { person ->
+            AdapterList(data = peopleState.value) { person ->
                 Row(person)
             }
-
         }
     }
 }
@@ -74,4 +73,24 @@ fun DefaultPreview() {
     MaterialTheme {
         Row(Assignment("ISS", "John O'Reilly"))
     }
+}
+
+
+@Composable
+fun <T> Flow<T>.collectAsState(
+    initial: T,
+    context: CoroutineContext = Dispatchers.Main
+): MutableState<T> {
+    val state = state { initial }
+    onPreCommit(this, context) {
+        val job = CoroutineScope(context).launch {
+            collect {
+                FrameManager.framed {
+                    state.value = it
+                }
+            }
+        }
+        onDispose { job.cancel() }
+    }
+    return state
 }
